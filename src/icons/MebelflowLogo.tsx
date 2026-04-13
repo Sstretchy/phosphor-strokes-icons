9c7e051bcf09da69158a0cb0159422ede00e9a9b
Ольга <olala@192.168.1.74>
2026-04-13 19:45:10 +0400

    feat: add MaxLogo, MebelflowLogo, WhatsAppLogoFilled icons

---
+++ src/icons/MebelflowLogo.tsx
@@ -0,0 +1,25 @@
+import { createIcon } from "../create-icon.js";
+
+export const MebelflowLogo = createIcon("mebelflow-logo", [
+  [
+    "path",
+    {
+      d: "M8.20911 10.96L11.7913 7.35999V4.24L8.20911 7.83999L4.38812 4H2V16H4.14931V6.87999L8.20911 10.96Z",
+      fill: "currentColor",
+      stroke: "none"
+    }
+  ],
+  [
+    "path",
+    {
+      d: "M11.7909 9.04002L8.20871 12.64V15.76L11.7909 12.16L15.6119 16H18V4.00004H15.8507V13.12L11.7909 9.04002Z",
+      fill: "currentColor",
+      stroke: "none"
+    }
+  ]
+], {
+  viewBox: "0 0 20 20",
+  absoluteStrokeBase: 20
+});
+
+export default MebelflowLogo;
